a3aeee09e34fd33e827df9c366d2f68ba47ec173
fsvbach <bachmafy@cgpool1904.Informatik.Uni-Tuebingen.De>
2021-04-21 12:24:43 +0200

    add timer

--- main.py
+++ main.py
@@ -6,27 +6,29 @@ Created on Fri Apr  9 11:15:50 2021
 @author: fsvbach
 """
 
-
-from Code.Distances import WassersteinDistanceMatrix
-from Code.Simulation import HierarchicalGaussianMixture
-from Code.Visualization import plotHGM, plotHGM2, plotTSNE, plotMDS
+from Code.utils import Timer
 
 w_range    = [0, 0.25, 0.5, 0.75, 1]
 # w_range    = [0.5]
-experiment = "HIGHdim"
+experiment = "TEST"
 seed       = 13
 sklearn    = True
+timer      = Timer(experiment)
 
 if sklearn:
     from sklearn.manifold import TSNE, MDS
 else:
     from openTSNE import TSNE
     
+from Code.Distances import WassersteinDistanceMatrix
+from Code.Simulation import HierarchicalGaussianMixture
+from Code.Visualization import plotHGM, plotHGM2, plotTSNE, plotMDS
+
 mixture = HierarchicalGaussianMixture(seed=seed,
                                     datapoints=100, 
                                     samples=30, 
-                                    features=20, 
-                                    classes=9,
+                                    features=2, 
+                                    classes=3,
                                     ClassDistance=40,
                                     ClassVariance=80,
                                     DataVariance=5)
@@ -35,18 +37,18 @@ if mixture.F == 2:
     plotHGM(mixture, prefix=experiment)
     plotHGM2(mixture, prefix=experiment)
 
-print(f'{mixture._info()}\nCreated data in {time.perf_counter()-start} seconds.')
+timer.add(f'{mixture._info()}\nCreated data')
 
 for w in w_range:
     info   = (mixture.C, mixture.N, w, experiment)
     
     matrix = WassersteinDistanceMatrix(mixture.data_estimates(), w=w)
-    print(f'Computed matrix with w={w} in {time.perf_counter()-start} seconds.')
+    timer.add(f'Computed matrix with w={w}')
     
     plotTSNE(TSNE, matrix, info=info, sklearn=sklearn)
-    print(f'Done TSNE in { time.perf_counter()-start} seconds.')
+    timer.add('Done TSNE')
     
-    plotMDS(MDS, matrix, info=info)
-    print(f'Done MDS in { time.perf_counter()-start} seconds.')
+    # plotMDS(MDS, matrix, info=info)
+    # timer.add('Done MDS')
 
-print(f'Succesfully finished code in {time.perf_counter()-start} seconds.')
+timer.finish()
