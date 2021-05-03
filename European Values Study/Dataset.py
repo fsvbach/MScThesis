660557fe385db40845094517f02c894ff553ea5c
fsvbach <simmiasmobil@gmail.com>
2021-05-03 13:33:16 +0200

    dne presentation

--- European Values Study/Dataset.py
+++ European Values Study/Dataset.py
@@ -5,7 +5,7 @@ Created on Sat May  1 12:57:17 2021
 
 @author: fsvbach
 """
-
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -22,23 +22,25 @@ df.loc[df[question2]<0, question2] = np.NaN
 
 fig, ax = plt.subplots(figsize=(15,15))
 
-ax.scatter(df[question1], df[question2])
+# ax.scatter(df[question1], df[question2])
 
 classes = df[[group, label, question1, question2]].groupby(label)
+legend = []
 
 for label, C in classes:
     
     rgb = np.random.rand(3,)
-    ax.scatter(C[question1].mean(), C[question2].mean(), c=[rgb], label=label, s=100)
-       
+    # ax.scatter(C[question1].mean(), C[question2].mean(), c=[rgb], label=label, s=100)
+    legend.append(mpatches.Patch(color=rgb, label=label))
+    
     datapoints = C[[group, question1, question2]].groupby(group)
 
     for point, D in datapoints:
         
         ax.scatter(D[question1].mean(), D[question2].mean(), c=[rgb], s=20)
         
-ax.legend(bbox_to_anchor=[1.0, 0.5], loc='center left')
-fig.savefig('Plots/political landscape.svg')
+ax.legend(handles=legend, bbox_to_anchor=[1.0, 0.5], loc='center left')
+fig.savefig('Plots/test.svg')
 plt.show()
 plt.close()
         
