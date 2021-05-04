f0e20518b8d94cc5c8e4b4a79483b3a94b28ff1f
fsvbach <simmiasmobil@gmail.com>
2021-05-04 15:59:00 +0200

    created election plot

--- Election Results/main.py
+++ Election Results/main.py
@@ -12,20 +12,34 @@ import matplotlib.pyplot as plt
 
 from openTSNE import TSNE
 
-nonvoters = False
+nonvoters = True
+newtSNE   = False
+seed      = 13
+numparty  = 7
+
+farben = pd.read_csv('Data/BW/Farben.csv', delimiter=';', encoding='utf-8', header=0)
+farben = farben.iloc[0].to_dict()
 
 data = pd.read_csv('Data/BW/Kreise.csv', delimiter=';', encoding='ISO-8859-1', index_col='Wahlkreis')
-
+head = 'Land Baden-Württemberg'
+  
+title = 'Political landscape of Baden-Württemberg'
 total = data['Gültige Stimmen'] 
 if nonvoters:
     total = data['Wahlberechtigte']
     data['Nichtwähler'] = total - data['Gültige Stimmen'] 
+    farben['Nichtwähler'] = 'grey'
+    title += ' with Non-Voters'
     
-data = data.drop(columns=data.columns[:8]).divide(total, axis='rows').fillna(0)
+data.drop(columns=data.columns[:8], inplace=True)
+data.fillna(0, inplace=True)
+data.sort_values(head, axis=1, ascending=False, inplace=True)
+ 
+data = data.divide(total, axis='rows')*100
+average = data.loc[head]
 
-average = data.loc['Land Baden-Württemberg']
-
-data = data.drop(index='Land Baden-Württemberg') - average
+data = (data-average)
+data.drop(index=head, inplace=True)
 
 def plotWahlkreis(district):
     name = ' '.join(data.loc[district].name.split(' ')[1:])
@@ -34,16 +48,57 @@ def plotWahlkreis(district):
     ax.set(ylabel='%',
            title=name)
     ax.tick_params(axis='x', labelrotation = 90)
+    fig.savefig(f'Plots/Wahlkreise/{district}.png')
     plt.show()
     plt.close()
+
+def plotBar(values, name):
+    fig, ax = plt.subplots()
+    ax.bar(data.columns, values)
+    ax.set(ylabel='%',
+           title=name)
+    ax.tick_params(axis='x', labelrotation = 90)
+    fig.savefig(f'Plots/Wahlkreise/{name}.png')
+    plt.show()
+    plt.close()
+
+embedding = np.load(f'Data/BW/embedding{nonvoters}.npy')
+if newtSNE:
+    tsne = TSNE(random_state=seed)
+    embedding = tsne.fit(data)
+X,Y = embedding.T
     
-# for i in data.index:
-#     plotWahlkreis(i)
+fig, ax = plt.subplots(figsize=(15,15))
 
-tsne = TSNE()
+# box of wahlkreis result
+f = 30
+g = 50
+k = 20
+s = 3
 
-embedding = tsne.fit(data)
+l = 3
+
+dist = k/numparty
+for i in range(numparty):
+    party = data.columns[i]
+    color = farben[party]
+    offset = (numparty/2-i)*dist-dist/2
+    ax.bar(f*X-offset, s*data[party], bottom=g*Y, 
+                           width=dist, color=color, align='center')
     
-x,y =embedding.T
+    ax.bar(f*min(X)+l*dist*i, average[i]*s, bottom=g*min(Y),
+           color=color, width=l*dist, align='edge')
 
-plt.scatter(x,y)
+for name, x, y in zip(data.index, X, Y):
+    text = ' '.join(name.split(' ')[1:])
+    sub = data.loc[name].max()
+    ax.annotate(text, (f*x-dist/2, g*y+sub*s), ha='center')
+    
+
+for spine in ax.spines:
+    ax.spines[spine].set(visible=False)
+ax.set(xticks=[],
+       yticks=[])
+ax.set_title(title, fontdict={'fontsize': 25})
+fig.savefig(f'Plots/Wahlkreise-NW{nonvoters}.svg')
+plt.show()
