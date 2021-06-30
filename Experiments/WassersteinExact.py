645458f843047e9f300174fa7ee684a6d53ad44b
fsvbach <simmiasmobil@gmail.com>
2021-06-30 16:39:07 +0200

    exact wasserstein

--- Experiments/WassersteinExact.py
+++ Experiments/WassersteinExact.py
@@ -12,15 +12,37 @@ from scipy.optimize import linprog
 from scipy.stats import wasserstein_distance
 from Datasets import EVS2020 as Data
 
+def EuclideanDistance(A,B):
+    N1 = np.linalg.norm(A, ord=2, axis=1).reshape(-1,1)**2 
+    N2 = np.linalg.norm(B, ord=2, axis=1).reshape(1,-1)**2 
+    N3 = -2 * np.inner(A,B)
+    D  = N1 + N2 + N3
+    D[np.where(D<0)] = 0
+    return np.sqrt(D)
+    
+def ConstraintMatrix(n,m):
+    N = np.repeat(np.identity(n), m, axis=1)
+    M = np.hstack([np.identity(m)]*n)
+    return np.vstack([N,M])
+    
 dataset, labels = Data.LoadEVS(Data.small, 
                                countries=None,
                                transform=False, 
                                NUTS=1,
                                min_entries=40) 
 
-A = dataset.loc['DE1'].values
-B = dataset.loc['AL0'].values
+A = dataset.loc['DE1'].values[:200]
+B = dataset.loc['AL0'].values[:50]
 
-D = np.sqrt(np.linalg.norm(A, ord=2, axis=1).reshape(-1,1)**2 +
-            np.linalg.norm(B, ord=2, axis=1).reshape(1,-1)**2 -
-            2 * np.inner(A,B))
+D = EuclideanDistance(A, B)
+plt.imshow(D)
+plt.show()
+
+n, m = len(A), len(B)
+
+A = ConstraintMatrix(n,m)
+b = np.concatenate([np.ones(n)/n, np.ones(m)/m])
+c = D.reshape(-1)
+
+opt_res = linprog(-b, A.T, c, bounds=[None, None])
+emd = -opt_res.fun
